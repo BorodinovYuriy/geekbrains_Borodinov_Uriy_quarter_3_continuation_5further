50bbf3c46c8efa2d86f3311e08eb1a454de65d15
Yuriy_Borodinov <aditone2378@gmail.com>
2022-05-23 20:33:58 +0300

    lesson_5 -Переделан метод сохранения истории -Попробовал реализовать управление сервера через ExecutorService -Переделано задание с гонками

---
+++ src/main/java/lesson_5_races/Tunnel.java
@@ -0,0 +1,30 @@
+package lesson_5_races;
+
+public class Tunnel extends Stage {
+    Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+    @Override
+    public void go(Car c) {
+        try {
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+
+                MainClass.tunnel_S.acquire();//!
+
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+
+                MainClass.tunnel_S.release();//!
+
+                System.out.println(c.getName() + " закончил этап: " + description);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.csbd {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
+    requires org.apache.commons.io;
+
+    opens com.example.csbd to javafx.fxml;
+    exports com.example.csbd;
+}
